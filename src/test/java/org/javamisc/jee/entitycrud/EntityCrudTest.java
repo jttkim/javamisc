3eb60201fdf83a4b07144b3153ebdd1031a6b027
Jan T Kim <jttkim@gmail.com>
2010-06-23 17:04:41 +0000

    added entitycrud test

---
+++ src/test/java/org/javamisc/jee/entitycrud/EntityCrudTest.java
@@ -0,0 +1,19 @@
+package org.javamisc.jee.entitycrud;
+
+import org.junit.*;
+
+import java.util.Set;
+import java.util.HashSet;
+
+
+public class EntityCrudTest
+{
+  @Test
+  public void testGenericTypecast()
+  {
+    Assert.assertTrue("something".equals(BeanUtil.extractPropertyName("getSomething")));
+    Assert.assertTrue("something".equals(BeanUtil.extractPropertyName("setSomething")));
+    Assert.assertTrue("something".equals(BeanUtil.extractPropertyName("isasdsomething")));
+  }
+}
+
