da9a9e20be233277dccc7f2182ea60bb98d606e5
Jan T Kim <jttkim@gmail.com>
2010-07-13 13:34:34 +0000

    more csv testing

--- src/test/java/org/javamisc/csv/CsvTest.java
+++ src/test/java/org/javamisc/csv/CsvTest.java
@@ -23,15 +23,53 @@ public class CsvTest
   }
 
 
+  private static String csvTextFunction(int i)
+  {
+    String text = "this is just some test";
+    if (i % 4 == 1)
+    {
+      text = "\"this is quoted text\"";
+    }
+    else if (i % 4 == 2)
+    {
+      text = "\"this has \"\"escaped\"\" quotes\"";
+    }
+    else if (i % 4 == 3)
+    {
+      text = "this \"a\"ppears\"\" to have an escaped quote";
+    }
+    return (text);
+  }
+
+
+  private static String plainTextFunction(int i)
+  {
+    String text = "this is just some test";
+    if (i % 4 == 1)
+    {
+      text = "this is quoted text";
+    }
+    else if (i % 4 == 2)
+    {
+      text = "this has \"escaped\" quotes";
+    }
+    else if (i % 4 == 3)
+    {
+      text = "this appears to have an escaped quote";
+    }
+    return (text);
+  }
+
+
   @Before
   public void setUp()
   {
     Random rng = new Random(4711);
-    this.xyData = "x,y\n";
+    this.xyData = "i,x,y,s\n";
     for (int i = 0; i < 10; i++)
     {
       int x = rng.nextInt(10);
-      this.xyData += String.format("%d,%d\n", x, xyFunction(x));
+      this.xyData += String.format("%d,%d,%d,%s\n", i, x, xyFunction(x), csvTextFunction(i));
     }
   }
 
@@ -47,15 +85,20 @@ public class CsvTest
   {
     BufferedReader xyReader = this.getXYReader();
     CsvTable t = new CsvTable(new CsvReader(xyReader));
-    Assert.assertEquals(t.getColumnNameList()[0], "x");
-    Assert.assertEquals(t.getColumnNameList()[1], "y");
+    Assert.assertEquals(t.getColumnNameList()[0], "i");
+    Assert.assertEquals(t.getColumnNameList()[1], "x");
+    Assert.assertEquals(t.getColumnNameList()[2], "y");
+    Assert.assertEquals(t.getColumnNameList()[3], "s");
     while (t.next())
     {
-      Assert.assertEquals(t.getString(0), t.getString("x"));
-      Assert.assertEquals(t.getString(1), t.getString("y"));
+      Assert.assertEquals(t.getString(1), t.getString("x"));
+      Assert.assertEquals(t.getString(2), t.getString("y"));
+      int i = Integer.parseInt(t.getString("i"));
       int x = Integer.parseInt(t.getString("x"));
       int y = Integer.parseInt(t.getString("y"));
+      String s = t.getString("s");
       Assert.assertEquals(y, xyFunction(x));
+      Assert.assertEquals(s, plainTextFunction(i));
     }
   }
 }
