881b4c6e94a6b38c15bbd9a4a30cab65edbd8831
Jan T Kim <jttkim@gmail.com>
2010-08-11 20:27:54 +0000

    added safeStr and splitTrim methods to Util

--- src/main/java/org/javamisc/Util.java
+++ src/main/java/org/javamisc/Util.java
@@ -15,6 +15,120 @@ public class Util
 
 
   /**
+   * Produce a string representation of a {@code String}, representing
+   * a {@code null} reference as "&lt;null&gt;".
+   *
+   * <p>It is safe to call this method without checking that the
+   * parameter is not {@code null}.</p>
+   *
+   * <p>Notice, however, that a string that contains the value {@code
+   * &lt;null&gt;} has a representation that cannot be distinguished
+   * from that of a {@code null} reference.</p>
+   *
+   * @param s the string variable to be represented
+   * @return the representation
+   */
+  public static String safeStr(String s)
+  {
+    if (s == null)
+    {
+      return ("<null>");
+    }
+    else
+    {
+      return (s);
+    }
+  }
+
+
+  /**
+   * Split a string into words and trim leading and trailing whitespace.
+   *
+   * @param s the string to be split
+   * @param regex the delimiting regular expression
+   * @return an array containing the trimmed words
+   */
+  public static String[] splitTrim(String s, String regex)
+  {
+    String w[] = s.split(regex);
+    for (int i = 0; i < w.length; i++)
+    {
+      w[i] = w[i].trim();
+    }
+    return (w);
+  }
+
+
+  /**
+   * Produce a string representation of an {@code Integer},
+   * representing a {@code null} reference as "&lt;null&gt;".
+   *
+   * <p>It is safe to call this method without checking that the
+   * parameter is not {@code null}.</p>
+   *
+   * @param i the variable to be represented
+   * @return the representation
+   */
+  public static String safeStr(Integer i)
+  {
+    if (i == null)
+    {
+      return ("<null>");
+    }
+    else
+    {
+      return (i.toString());
+    }
+  }
+
+
+  /**
+   * Produce a string representation of an {@code Boolean},
+   * representing a {@code null} reference as "&lt;null&gt;".
+   *
+   * <p>It is safe to call this method without checking that the
+   * parameter is not {@code null}.</p>
+   *
+   * @param b the variable to be represented
+   * @return the representation
+   */
+  public static String safeStr(Boolean b)
+  {
+    if (b == null)
+    {
+      return ("<null>");
+    }
+    else
+    {
+      return (b.toString());
+    }
+  }
+
+
+  /**
+   * Produce a string representation of a {@code Double},
+   * representing a {@code null} reference as "&lt;null&gt;".
+   *
+   * <p>It is safe to call this method without checking that the
+   * parameter is not {@code null}.</p>
+   *
+   * @param d the variable to be represented
+   * @return the representation
+   */
+  public static String safeStr(Double d)
+  {
+    if (d == null)
+    {
+      return ("<null>");
+    }
+    else
+    {
+      return (d.toString());
+    }
+  }
+
+
+  /**
    * Helper method to cast values to parameterised types without
    * triggering an "unchecked" warning.
    *
--- src/test/java/org/javamisc/JavamiscTest.java
+++ src/test/java/org/javamisc/JavamiscTest.java
@@ -18,5 +18,40 @@ public class JavamiscTest
     castSet.add(testString);
     Assert.assertTrue(castSet.contains(testString));
   }
-}
 
+
+  @Test
+  public void testSafeStr()
+  {
+    String s = null;
+    Assert.assertEquals(Util.safeStr(s), "<null>");
+    s = "blah";
+    Assert.assertEquals(Util.safeStr(s), s);
+    Integer i = null;
+    Assert.assertEquals(Util.safeStr(i), "<null>");
+    i = new Integer(4711);
+    Assert.assertEquals(Util.safeStr(i), i.toString());
+    Boolean b = null;
+    Assert.assertEquals(Util.safeStr(b), "<null>");
+    b = new Boolean(false);
+    Assert.assertEquals(Util.safeStr(b), b.toString());
+    Double d = null;
+    Assert.assertEquals(Util.safeStr(d), "<null>");
+    d = new Double(47.11);
+    Assert.assertEquals(Util.safeStr(d), d.toString());
+  }
+
+
+  @Test
+  public void splitTrimTest()
+  {
+    String s = "  this, stuff, was ,   written   , in, Monteria";
+    String[] w = Util.splitTrim(s, ",");
+    Assert.assertEquals(w[0], "this");
+    Assert.assertEquals(w[1], "stuff");
+    Assert.assertEquals(w[2], "was");
+    Assert.assertEquals(w[3], "written");
+    Assert.assertEquals(w[4], "in");
+    Assert.assertEquals(w[5], "Monteria");
+  }
+}
